3c62a56fa3971aad60ee8009e407c6fba181c5eb
jrgmicael-creator <jrgmicael@gmail.com>
2025-09-07 13:36:14 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import streamlit as st
 import pandas as pd
 import gspread
 from google.oauth2.service_account import Credentials
-
+import qrcode
+from io import BytesIO
 # =======================
 # Configuração Google Sheets
 # =======================
